e922171a92350ad38232b2bd414e58117da7a653
Anton Karpenko <karpolan@karpolan.com>
2024-01-17 03:42:38 +0100

    fix: code review

--- src/app/(styled)/[...slug]/2011-09-30-legal-documents-added.tsx
+++ src/app/(styled)/[...slug]/2011-09-30-legal-documents-added.tsx
@@ -5,7 +5,9 @@ export const tags = ['website', 'legal', 'document'];
 export const title = 'Legal documents added';
 export const content = (
   <>
-    <Typo variant="paragraph">Legal documents have been added to our site:</Typo>
+    <Typo variant="paragraph">
+      <Link href="/legal/">Legal documents</Link> have been added to our site:
+    </Typo>
     <Typo variant="list">
       <li>
         <Link href="/legal/legal-notices/">Legal notices</Link>
