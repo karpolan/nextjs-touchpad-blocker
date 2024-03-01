4cfa801e04b126a21fe48284533c68a3220a82eb
Anton Karpenko <karpolan@karpolan.com>
2024-03-01 03:26:02 +0100

    fix: typos feat: new content in news, articles, software fix: pad.xml download links changes to S3 bucket

--- src/app/(main)/[...slug]/2024-01-15-new-website.tsx
+++ src/app/(main)/[...slug]/2024-01-15-new-website.tsx
@@ -6,7 +6,7 @@ export const title = 'New Website';
 export const content = (
   <>
     <Typo variant="paragraph">
-      We recrate our website from scratch, using <Link href="https://nextjs.org/">Next.js</Link> framework.
+      We re-create our website from scratch, using <Link href="https://nextjs.org/">Next.js</Link> framework.
     </Typo>
     <Typo variant="paragraph">It is a static website now. It is fast, secure and easy to maintain.</Typo>
   </>
